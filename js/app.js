f706b0f24e5994e471814831822b93b8964f3d10
ha-nakamura112 <nh.3213.b.b@gmail.com>
2022-10-11 06:58:54 +0900

    update nav,project react

--- js/app.js
+++ js/app.js
@@ -1,8 +1,12 @@
 const sections = document.querySelectorAll('.section');
 const sectBtns = document.querySelectorAll('.controlls');
+const smallBtns = document.querySelectorAll('.small-controlls');
 const sectBtn = document.querySelectorAll('.control');
 const allSections = document.querySelector('.main-content');
 const switchBtn = document.querySelector('.switch-btn');
+const switchBtns = document.querySelectorAll('.switch-color');
+// const detalsbtn = document.getElementsByTagName('details')[0];
+
 
 //
 
@@ -35,9 +39,37 @@ function pageColorChg(){
     }
   })
 
+  for(let i = 0; i < smallBtns .length; i++){
+    smallBtns[i].addEventListener('click', function(){
+      let currentBtn = document.querySelectorAll('.active-btn');
+      currentBtn[0].className = currentBtn[0].className.replace('active-btn', '');
+      this.className += 'active-btn';
+    })
+  }
+  allSections.addEventListener('click', (e) =>{
+    let id = e.target.dataset.id;
+    if(id){
+      smallBtns.forEach((btn)=>{
+        btn.classList.remove('active-btn');
+      })
+      e.target.classList.add('active-btn')
+
+      sections.forEach((section)=>{
+        section.classList.remove('active');
+      })
+      let element = document.getElementById(id);
+      element.classList.add('active');
+    }
+  })
+
   switchBtn.addEventListener('click', ()=>{
     let element = document.body;
     element.classList.toggle('light-mode');
+    
+    switchBtns.forEach((btns)=>{
+      btns.classList.toggle('hide');
+    })
+
   })
 
 
