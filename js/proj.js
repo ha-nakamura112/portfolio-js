0fbc4df697961e6119bcf20f2847fd185907fe31
ha-nakamura112 <nh.3213.b.b@gmail.com>
2022-11-06 06:11:09 +0900

    upadated

--- js/proj.js
+++ js/proj.js
@@ -4,7 +4,6 @@ const projs = document.querySelectorAll('.port-item');
 
 
 function filterProj(){
-  // console.log(document.querySelectorAll('.port-text')[0])
   for(let i = 0; i < sortBtn.length; i++){
     sortBtn[i].addEventListener('click', function(){
       let currentBtn = document.querySelectorAll('.act-btn');
@@ -12,13 +11,9 @@ function filterProj(){
       this.className += 'act-btn';
 
       let showBtn = document.querySelectorAll('.act-btn');
-      // console.log(showBtn);
       let val = showBtn[0].className.split(" ")[1];
-      // console.log(val);
       
         projs.forEach((proj)=>{
-          // console.log(proj);
-          // console.log(proj.classList);
           proj.className = proj.className.replace('hide', '');
           if(val != 'all'){
             if( proj.classList.contains(val) == false ){
